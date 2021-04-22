f75c605bffc5f4dc9b0728e65726796aa74315c9
FrancoCaneti <CAnetifranco95@hmail.com>
2021-04-22 18:14:38 +0200

    update

--- script.js
+++ script.js
@@ -14,8 +14,33 @@ const app = new Vue ({
          newTodo:''
      },
      methods: {
+         //inserisci un nuovo elemento
          addTodo() {
-             console.log('add')
-         }
+             console.log('add');
+
+             if(this.newTodo.trim() !== '') {
+                 this.todos.push ({
+                     text: this.newTodo,
+                     completed: false,
+                 });
+                 this.newTodo = '';
+                 this.$refs.todoInput.focus();
+             }
+         },
+            //remove Todo
+
+            @param {number} indexed
+           removeTodo(index) { 
+               this.todos.splice(index,1);
+               const deleted = this.todos.splice(index,1) [0];
+           }
+
+           //Update todo
+          @ param   {number} index
+           updateStatus(imdex) {
+               this.todos[index].completed = ! this.todos[index].
+               completed;
+           }
+        
      }
 });
