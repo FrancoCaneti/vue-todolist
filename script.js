ac54d0253eebff81940afacd008f684cf0346fd7
FrancoCaneti <CAnetifranco95@hmail.com>
2021-04-22 17:42:11 +0200

    todo

--- script.js
+++ script.js
@@ -1,40 +1,21 @@
 const app = new Vue ({
     el: '#app',
-    data:{
-        todos:[
-            {
-              text:'Fare la spesa',
-              completed:false,
-            } ,
-
-            {
-              text:'Pulire casa',
-              completed:false,
-            } ,
-
-            {
-              text:'Portare fuori il cane',
-              completed:false,
-             } ,
-        ],
-        newTodo:'',
-        
-    
-    },
-    methods: {
-        //inserire in nuovo todo nella lista
-
-        addTodo(){
-
-
-            if(this.newTodo !== ''){
-                this.todos.push({
-                    text:this.newTodo,
-                    completed:false
-                });
-                this.newTodo ='';
-                this.$refs.todoInput.focus();
-            }
-        }
-    }
+     data : {
+         todos: [
+             {
+                 text:'ciao',
+                 completed: false,
+             },
+             {
+                 text:'come va',
+                 completed:false,
+             },
+         ],
+         newTodo:''
+     },
+     methods: {
+         addTodo() {
+             console.log('add')
+         }
+     }
 });
