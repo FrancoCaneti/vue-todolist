625e32c7f30903ee1d2451645836f5a6178c7448
FrancoCaneti <CAnetifranco95@hmail.com>
2021-04-22 16:08:07 +0200

    elemnt

---
+++ script.js
@@ -0,0 +1,27 @@
+const app = new Vue ({
+    el: '#app',
+    data:{
+        todos:[
+            {
+              text:'Fare la spesa',
+              completata:false,
+            } ,
+
+            {
+              text:'Fare la spesa',
+              completata:false,
+            } ,
+
+            {
+              text:'Fare la spesa',
+              completata:false,
+             } ,
+        ],
+        newTodo:'',
+    },
+    methods: {
+        //inserire in nuovo todo nella lista
+
+        addTodo(): 
+    }
+});
